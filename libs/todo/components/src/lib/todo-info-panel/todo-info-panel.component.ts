3693cf40713230243ae408a792d4474c5c5c26cf
bagreen-89 <60008187+bagreen-89@users.noreply.github.com>
2024-05-29 15:43:20 +0200

    last stylistic changes

--- libs/todo/components/src/lib/todo-info-panel/todo-info-panel.component.ts
+++ libs/todo/components/src/lib/todo-info-panel/todo-info-panel.component.ts
@@ -1,13 +1,11 @@
 import { Component, ViewEncapsulation, inject } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { ToDoStore } from '@todo-app-ha/todo/data-access';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'ha-todo-info',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatCheckboxModule],
+  imports: [MatCardModule],
   templateUrl: './todo-info-panel.component.html',
   styleUrl: './todo-info-panel.component.scss',
   encapsulation: ViewEncapsulation.None,
